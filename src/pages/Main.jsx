559ce084096960d3ba45745097716cacc6a15a11
play93 <95739833+play93@users.noreply.github.com>
2024-08-29 15:20:50 +0900

    feat : 홈 레이아웃 추가 - Tab 기능 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,39 @@
-import { useContext } from "react"
-import { postContext } from "../contexts/PostContext"
-const Main = () => {
+import { useContext } from "react";
+import { postContext } from "../contexts/PostContext";
+import { useState } from "react";
 
+const Main = () => {
   // context Test
   const { testList, addTestListHandler } = useContext(postContext);
   console.log("testList =>", testList);
 
+  const TabData = [
+    { id: 1, button: "답변 전", content: "~~답변을 기다리는 질문들~~" },
+    { id: 2, button: "답변 후", content: "~~답변이 끝난 질문들~~~" },
+  ];
+
+  const [activeTab, setActiveTab] = useState(TabData[0].id);
+
   return (
     <>
-      메인페이지
+      <h1>Latest post</h1>
+      <div>
+        <div>
+          {TabData.map((tab) => (
+            <button
+              key={tab.id}
+              data-active={activeTab === tab.id ? "true" : "false"}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.button}
+            </button>
+          ))}
+        </div>
+        <div>{TabData.find((a) => a.id === activeTab)?.content}</div>
+      </div>
       <button onClick={addTestListHandler}>테스트 </button>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
