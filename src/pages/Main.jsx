b03dca474d2883dd1a48c669d3183c6e221bde1f
play93 <95739833+play93@users.noreply.github.com>
2024-08-29 19:00:12 +0900

    feat : 게시물 리스트 가져오기 - PostListItem 컴포넌트 생성해 홈으로 불러오기

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,39 +1,53 @@
 import { useContext } from "react";
 import { postContext } from "../contexts/PostContext";
 import { useState } from "react";
+import styled from "styled-components";
+import PostListItem from "../components/PostListItem";
 
 const Main = () => {
   // context Test
-  const { testList, addTestListHandler } = useContext(postContext);
-  console.log("testList =>", testList);
-
+  const { testList } = useContext(postContext);
+  //console.log("testList =>", testList);
+  console.log(PostListItem.post);
   const TabData = [
-    { id: 1, button: "답변 전", content: "~~답변을 기다리는 질문들~~" },
+    { id: 1, button: "답변 전", content: <PostListItem /> },
     { id: 2, button: "답변 후", content: "~~답변이 끝난 질문들~~~" },
   ];
 
   const [activeTab, setActiveTab] = useState(TabData[0].id);
-
   return (
     <>
-      <h1>Latest post</h1>
-      <div>
-        <div>
-          {TabData.map((tab) => (
-            <button
-              key={tab.id}
-              data-active={activeTab === tab.id ? "true" : "false"}
-              onClick={() => setActiveTab(tab.id)}
-            >
-              {tab.button}
-            </button>
-          ))}
-        </div>
-        <div>{TabData.find((a) => a.id === activeTab)?.content}</div>
-      </div>
-      <button onClick={addTestListHandler}>테스트 </button>
+      <HomePostListTitle>Latest post</HomePostListTitle>
+
+      <TabButtonWrap>
+        {TabData.map((tab) => (
+          <TabButton
+            key={tab.id}
+            data-active={activeTab === tab.id ? "true" : "false"}
+            onClick={() => setActiveTab(tab.id)}
+          >
+            {tab.button}
+          </TabButton>
+        ))}
+      </TabButtonWrap>
+      <div>{TabData.find((a) => a.id === activeTab)?.content}</div>
     </>
   );
 };
 
 export default Main;
+
+const HomePostListTitle = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+`;
+const TabButtonWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`;
+
+const TabButton = styled.button`
+  background-color: pink;
+  cursor: pointer;
+`;
