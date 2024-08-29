3c9f9f85fdd8a19d26ae861a316a1d39380e687e
Your Name <you@example.com>
2024-08-29 14:24:51 +0900

    브랜치 생성

---
+++ src/components/LoginDiv.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginDiv = () => {
+  return <div>LoginDiv</div>;
+};
+
+export default LoginDiv;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,27 @@
-import { useContext } from "react"
-import { dataContext } from "../contexts/DataContext"
+import { useContext } from "react";
+import { dataContext } from "../contexts/DataContext";
 import { useEffect } from "react";
 import supabase from "../services/supabaseClient";
+import CreateAccount from "../components/CreateAccount";
+import LoginDiv from "../components/LoginDiv";
 
 const Login = () => {
-  const {users, setUsers, posts} = useContext(dataContext);
+  const { users, setUsers, posts } = useContext(dataContext);
 
   useEffect(() => {
     const fetchData = async () => {
-      const {data, error} = await supabase.from("Users").select("*");
-      if(error) {
+      const { data, error } = await supabase.from("Users").select("*");
+      if (error) {
         console.log("error =>", error);
-      }else{
+      } else {
         console.log("user data =>", data);
         setUsers(data);
       }
-    }
+    };
     fetchData();
   }, []);
 
-  return (
-    <>
-      로그인 페이지
-    </>
-  )
-}
+  return <>{true ? <LoginDiv /> : <CreateAccount />}</>;
+};
 
-export default Login
+export default Login;
