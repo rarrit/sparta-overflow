f8cc59dc7f89936f5d8b73db0d93fb722d001659
limbora <lbrkr96@naver.com>
2024-08-29 13:14:02 +0900

    test : 테스트 커밋

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,25 @@
-import { useContext } from "react"
-import { dataContext } from "../contexts/DataContext"
+import { useContext } from "react";
+import { dataContext } from "../contexts/DataContext";
 import { useEffect } from "react";
 import supabase from "../services/supabaseClient";
 
 const Login = () => {
-  const {users, setUsers, posts} = useContext(dataContext);
+  const { users, setUsers, posts } = useContext(dataContext);
 
   useEffect(() => {
     const fetchData = async () => {
-      const {data, error} = await supabase.from("Users").select("*");
-      if(error) {
+      const { data, error } = await supabase.from("Users").select("*");
+      if (error) {
         console.log("error =>", error);
-      }else{
+      } else {
         console.log("user data =>", data);
         setUsers(data);
       }
-    }
+    };
     fetchData();
   }, []);
 
-  return (
-    <>
-      로그인 페이지
-    </>
-  )
-}
+  return <>로그인 페이지</>;
+};
 
-export default Login
+export default Login;
