53ac82ea6e154b67965230c44c6885871ccf51aa
Your Name <you@example.com>
2024-08-29 19:34:20 +0900

    pull 이전 commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,17 @@
-import { useContext } from "react"
-import { postContext } from "../contexts/PostContext"
+import { useContext } from "react";
+import { postContext } from "../contexts/PostContext";
 const Main = () => {
-
   // context Test
-  const { testList, addTestListHandler } = useContext(postContext);
+  const { testList, addTestListHandler, loginUser } = useContext(postContext);
   console.log("testList =>", testList);
+  console.log("user", loginUser);
 
   return (
     <>
       메인페이지
       <button onClick={addTestListHandler}>테스트 </button>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
