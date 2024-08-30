04b76360482ee27d441086f7643e09f79e7d540a
Your Name <you@example.com>
2024-08-30 16:17:41 +0900

    refactor:로그인한 사람의 정보를 id로만 저장하던 것을 객체로 저장 -DataContext 대폭 수정

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { createContext } from "react";
 import supabase from "../services/supabaseClient";
-import { useNavigate } from "react-router-dom";
 
 export const dataContext = createContext();
 export const DataProvider = ({ children }) => {
@@ -9,31 +8,44 @@ export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
   // 로그인 관련 Hook
-  const [isLogin, ChangeLogin] = useState(false);
-  const [loginUserId, setLoginUserId] = useState("");
+  const [loginUserInfo, setLoginUserInfo] = useState("");
+  const [isLogin, changeLogin] = useState(false);
   // 로그인 페이지에서 로그인을 하면 loginUserId에 로그인한 유저의 uid가 저장됨
-  console.log("loginUserId=>", loginUserId);
+  console.log("loginUserInfo=>", loginUserInfo);
   console.log("isLogin=>", isLogin);
+
+  // 이미 가입한 유저 정보 가져오기
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("userinfo").select("*");
+      if (error) {
+        console.log("user data error =>", error);
+      } else {
+        console.log("user data =>", data);
+        setUsers(data);
+      }
+    };
+    fetchData();
+  }, []);
 
   // 로그인과 관련된 동작이 일어나면 이 안에 있는 함수가 실행됨
   const { loginAuthData } = supabase.auth.onAuthStateChange(
     (event, session) => {
-      // console.log(event, session);
-
       if (event === "INITIAL_SESSION") {
         // handle initial session
       } else if (event === "SIGNED_IN") {
         // handle sign in event
-        setLoginUserId(session.user.id);
-
+        const selectLoginUserInfo = users.filter(
+          (user) => user.id === session.user.id
+        );
+        setLoginUserInfo(selectLoginUserInfo);
         // 로그인 상태 변경
-        ChangeLogin(true);
+        changeLogin(true);
       } else if (event === "SIGNED_OUT") {
         // handle sign out event
-        setLoginUserId("");
-
+        setLoginUserInfo([]);
         // 로그인 상태 변경
-        ChangeLogin(false);
+        changeLogin(false);
       } else if (event === "PASSWORD_RECOVERY") {
         // handle password recovery event
       } else if (event === "TOKEN_REFRESHED") {
@@ -43,6 +55,7 @@ export const DataProvider = ({ children }) => {
       }
     }
   );
+
   return (
     <dataContext.Provider
       value={{
@@ -53,7 +66,7 @@ export const DataProvider = ({ children }) => {
         comments,
         setComments,
         isLogin,
-        ChangeLogin,
+        changeLogin,
         loginAuthData,
       }}
     >
