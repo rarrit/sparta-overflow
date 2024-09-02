992517cc5b07f92c93992ec930bafb21d15efc6a
play93 <95739833+play93@users.noreply.github.com>
2024-09-02 21:03:01 +0900

    Fix : 코드 리팩토링 - 게시글 정보 & 작성자 정보 가져오는 코드 병합 - 이미지 alt 개선 - SyntaxHighlighter 사용하지 않는 옵션 개선

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,18 +1,18 @@
-import { useContext } from "react";
-import { dataContext } from "../contexts/DataContext";
-import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import supabase from "../services/supabaseClient";
-import { CircleCheck, CircleX, Copy, CheckCheck } from "lucide-react";
-import { filterDateOnlyYMD } from "../utils/dateInfoFilter";
-import hljs from "highlight.js";
 
+import { dataContext } from "../contexts/DataContext";
+import { filterDateOnlyYMD } from "../utils/dateInfoFilter";
+import PostWrite from "./PostWrite";
+import supabase from "../services/supabaseClient";
+
+import styled from "styled-components";
+import { CircleCheck, CircleX, Copy, CheckCheck } from "lucide-react";
+import hljs from "highlight.js";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { railscasts } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import PostWrite from "./PostWrite";
-import { Viewer } from '@toast-ui/react-editor';
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import { Viewer } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 
 const PostDetail = () => {
   const navigate = useNavigate();
@@ -56,11 +56,17 @@ const PostDetail = () => {
   const { loginUserInfo } = useContext(dataContext);
 
   useEffect(() => {
-    //게시글 정보
-    const fetchPosts = async () => {
+    //게시글 정보 & 작성자 정보 & 댓글 갯수
+    const fetchPostAndAuthorAndComment = async () => {
       const { data, error } = await supabase
         .from("Post")
-        .select("*")
+        .select(
+          `*,
+          userInfo:userId(
+            id, created_at, email, username, profileImage
+          ),
+          Comment:postId (id)`
+        )
         .eq("id", id)
         .single();
       console.log(post);
@@ -69,39 +75,12 @@ const PostDetail = () => {
       } else {
         console.log("post data =>", data);
         setPosts(data);
-
-        // 작성자 정보 로드
-        if (data.userId) {
-          fetchAuthor(data.userId);
-        }
+        setUserInfo(data.userinfo);
+        setComment(data.Comment);
       }
     };
-
-    //작성자 정보
-    const fetchAuthor = async (userId) => {
-      const { data, error } = await supabase
-        .from("userinfo")
-        .select("id, created_at, email, username, profileImage")
-        .eq("id", userId)
-        .single();
-      if (error) {
-        console.log("error =>", error);
-      } else {
-        console.log("post data =>", data);
-        setUserInfo(data);
-      }
-    };
-
-    fetchAuthor();
-    fetchPosts();
+    fetchPostAndAuthorAndComment();
   }, [id]);
-
-  console.log("post.description =>>>>>>>>", post)
-
-  // 임시 버튼
-  const writeCommentHandel = () => {
-    navigate(`/write/${id}`);
-  };
 
   console.log("Logged in user:", loginUserInfo);
   console.log("Post data:", post);
@@ -118,7 +97,10 @@ const PostDetail = () => {
             <StTitle>{post.title}</StTitle>
             <StSubWriteInfo>
               <StUser>
-                <img src={userInfo.profileImage} alt="프로필이미지" />
+                <img
+                  src={userInfo.profileImage}
+                  alt={`${userInfo.username}님의 이미지`}
+                />
                 <span>{userInfo.username}</span>
               </StUser>
               <StDate>
@@ -133,13 +115,12 @@ const PostDetail = () => {
 
         {/* 글 영역 */}
         <StDescArea>
-          
           <StDescription>
-          {post.description ? (
-            <Viewer initialValue={post.description} />
-          ) : (
-            <p>Loading...</p>
-          )}
+            {post.description ? (
+              <Viewer initialValue={post.description} />
+            ) : (
+              <p>Loading...</p>
+            )}
           </StDescription>
 
           <StCodeBox>
@@ -166,7 +147,6 @@ const PostDetail = () => {
               )}
             </StCodeBoxTopAreaWithCopyBtn>
             <SyntaxHighlighter
-              language="javascript"
               style={railscasts}
               customStyle={{
                 padding: "25px",
@@ -183,15 +163,21 @@ const PostDetail = () => {
       </StContainer>
 
       {loginUserInfo.id === post.userId ? (
-        <StFixedBtnArea>        
-          <StBtn className="btnLine" onClick={() => handleEditPostMove(post.id)}>수정</StBtn>
-          <StBtn className="btnBlack" onClick={() => handleDeletePost(post.id)}>삭제</StBtn>
+        <StFixedBtnArea>
+          <StBtn
+            className="btnLine"
+            onClick={() => handleEditPostMove(post.id)}
+          >
+            수정
+          </StBtn>
+          <StBtn className="btnBlack" onClick={() => handleDeletePost(post.id)}>
+            삭제
+          </StBtn>
         </StFixedBtnArea>
       ) : null}
     </>
   );
 };
-
 
 const StContainer = styled.div`
   padding: 60px 0 120px;
@@ -287,7 +273,7 @@ const StBtn = styled.button`
   height: 35px;
   font-weight: 500;
   text-align: center;
-  background:#fff;
+  background: #fff;
   border: 1px solid #666;
   border-radius: 5px;
 `;
@@ -322,31 +308,31 @@ const StCopyCodeBtn = styled.button`
 `;
 
 const StFixedBtnArea = styled.div`
-  position:fixed; 
-  width:100%;
-  left:0;
-  bottom:0;
-  padding:15px;
-  display:flex;
-  gap:10px;
-  box-shadow:.5px .5px 10px rgba(0,0,0,.15);
-  z-index:999;
-  background:#fff;
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  padding: 15px;
+  display: flex;
+  gap: 10px;
+  box-shadow: 0.5px 0.5px 10px rgba(0, 0, 0, 0.15);
+  z-index: 999;
+  background: #fff;
   button {
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    width:50%;
-    height:35px;
-    border:1px solid #111;
-    border-radius:5px;
-    cursor:pointer;
-    transition:all .15s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    height: 35px;
+    border: 1px solid #111;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.15s ease;
     &:hover {
-      color:#fff;
-      background:#111;      
+      color: #fff;
+      background: #111;
     }
   }
-`
+`;
 
 export default PostDetail;
